b91b85f22cbc111bf43fbf712c69bcacaaee1b32
enalmarzuki <enalmarzuki07@gmail.com>
2025-03-03 22:32:12 +0800

    learn: day 2 of golang web

---
+++ belajar-golang-web/8-form-post_test.go
@@ -0,0 +1,39 @@
+package belajar_golang_web
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+func FormPost(writer http.ResponseWriter, request *http.Request) {
+	err := request.ParseForm() // <- harus di ParseForm dlu (manual)
+	if err != nil {
+		panic(err)
+	}
+
+	// request.PostFormValue("first_name") // <- ini otomatis tanpa harus melakukan parseForm() terlebih dahulus
+
+	firstName := request.PostForm.Get("first_name")
+	lastName := request.PostForm.Get("last_name")
+
+	fmt.Fprintf(writer, "Hello %s %s", firstName, lastName)
+}
+
+func TestFormPost(t *testing.T) {
+	requestBody := strings.NewReader("first_name=Jhon&last_name=Doe")
+	request := httptest.NewRequest(http.MethodPost, "http://localhost:8080", requestBody)
+	request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+	recorder := httptest.NewRecorder()
+
+	FormPost(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	fmt.Println(string(body))
+}
