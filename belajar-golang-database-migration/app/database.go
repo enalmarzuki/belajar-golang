5161bf8f3d30c1e05d48f0397bcbffe3c3894736
enalmarzuki <enalmarzuki07@gmail.com>
2025-04-20 16:30:39 +0800

    learn: day 1 of database migration has completed

---
+++ belajar-golang-database-migration/app/database.go
@@ -0,0 +1,28 @@
+package app
+
+import (
+	"belajar-golang-restful-api/helper"
+	"database/sql"
+	"time"
+)
+
+func NewDB() *sql.DB {
+	db, err := sql.Open("mysql", "root:enal1233@tcp(localhost:3306)/belajar_golang_database_migration")
+	helper.PanicIfError(err)
+
+	db.SetConnMaxIdleTime(5)
+	db.SetMaxOpenConns(20)
+	db.SetConnMaxLifetime(60 * time.Minute)
+	db.SetConnMaxIdleTime(10 * time.Minute)
+
+	return db
+
+	// migrate create -ext sql -dir db/migrations create_table_first
+	// migrate create -ext sql -dir db/migrations create_table_second
+	// migrate create -ext sql -dir db/migrations create_table_third
+	// migrate create -ext sql -dir db/migrations sample_dirty_state
+	// migrate -database "mysql://root:enal1233@tcp(localhost:3306)/belajar_golang_database_migration" -path db/migrations up
+	// migrate -database "mysql://root:enal1233@tcp(localhost:3306)/belajar_golang_database_migration" -path db/migrations down
+	// migrate -database "mysql://root:enal1233@tcp(localhost:3306)/belajar_golang_database_migration" -path db/migrations version
+	// migrate -database "mysql://root:enal1233@tcp(localhost:3306)/belajar_golang_database_migration" -path db/migrations force 20250420081052
+}
---
+++ belajar-golang-database-migration/database.sql
@@ -0,0 +1,12 @@
+CREATE TABLE category (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(200) NOT NULL
+)  ENGINE=INNODB;
+
+SELECT * FROM category;
+
+TRUNCATE category;
+
+use belajar_golang_restful_api_test;
+
+use belajar_golang_restful_api_test;
